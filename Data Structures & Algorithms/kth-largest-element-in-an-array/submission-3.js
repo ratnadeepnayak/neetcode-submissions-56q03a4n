0c19f68b1a9737c9e0d3103a4df6052d97448582
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-04-22 07:42:31 -0400

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        const minHeap = new MinPriorityQueue();
+
+        for (let i= 0; i< nums.length; i++){
+            minHeap.enqueue(nums[i]);
+
+            if(minHeap.size() > k){
+                minHeap.dequeue();
+            }
+        }
+
+        return minHeap.front()
+    }
+}
