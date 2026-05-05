739c8bf444942a8c217e70a86f5329051d7713a1
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-05-05 05:21:54 -0400

    Add: max-water-container - submission-5

---
+++ Data Structures & Algorithms/max-water-container/submission-5.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let left = 0;
+        let right = heights.length - 1;
+        let area = 0;
+
+        while(left < right){
+            let currArea = (right - left) * (Math.min(heights[left], heights[right]))
+            area = Math.max(area,currArea);
+
+            if(heights[left] < heights[right]){
+                left++;
+            }else{
+                right--;
+            }
+
+        }
+
+        return area;
+    }
+}
