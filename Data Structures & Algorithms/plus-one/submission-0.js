97fdf612d8e9685b22f920f5d4fe67c2e6bcbf43
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-06-17 08:06:51 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+
+        let n = digits.length;
+
+        for (let i = n-1; i >=0; i--){
+
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }         
+        }
+
+        return [1, ...digits];
+
+    }
+}
