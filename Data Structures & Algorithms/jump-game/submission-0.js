d12acc5548d882439567690502bc8f39b7742b4b
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-06-22 16:32:07 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        let farthest = 0;
+        for (let i = 0; i< nums.length; i++){
+
+            if(i > farthest){
+                return false
+            }
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+        }
+
+        return true
+    }
+}
