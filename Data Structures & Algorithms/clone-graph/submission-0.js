a175b77704dc6ec08043d11eeef3199f894c4016
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-06-27 13:29:19 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+
+        if(!node) return null
+
+        let queue = [node];
+
+        let clonedNode = new Node(node?.val);
+        let visited = new Map();
+        visited.set(node, clonedNode);
+
+        while (queue.length > 0) {
+            let curr = queue.shift();
+
+            for (let n of curr.neighbors) {
+                if (!visited.has(n)) {
+                    visited.set(n, new Node(n.val));
+                    queue.push(n);
+                }
+
+                visited.get(curr).neighbors.push(visited.get(n));
+            }
+        }
+
+        return clonedNode
+    }
+}
