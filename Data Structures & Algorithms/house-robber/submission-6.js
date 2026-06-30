1f25ded38f193b23cc7960684062116ad740264b
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-06-30 06:55:14 -0400

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let dp = [nums[0], Math.max(nums[0], nums[1])]
+
+
+        for (let i=2; i<nums.length; i++){
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        }
+
+        return dp[nums.length - 1]
+    }
+}
