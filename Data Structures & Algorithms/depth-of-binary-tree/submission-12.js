5e27fc1fd6f5338ba521b3cf1be60b75a105f482
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-07-30 18:42:47 -0400

    Add: depth-of-binary-tree - submission-12

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-12.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+             1
+            / \
+           2.  3
+
+     */
+    maxDepth(root) {
+
+        let maxDepth = 0;
+
+        function dfs(node){
+
+            if(!node) return 0;
+
+            let left = dfs(node?.left);
+            let right = dfs(node?.right);
+
+            let depth =  1 + Math.max(left,right);
+
+            maxDepth = Math.max(depth, maxDepth);
+
+            return depth
+        }
+
+        dfs(root);
+
+        return maxDepth
+    }
+}
