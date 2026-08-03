d71105e65fef581cb5846dc8242977a7728ef487
Ratnadeep Nayak <nayak.ratnadeep@gmail.com>
2026-08-03 19:40:23 -0400

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let n = s.length;
+        let dp = Array.from({ length: n }, () => Array(n).fill(false));
+
+        let ans = 0;
+
+        for (let i = 0; i < n; i++) {
+            dp[i][i] = true;
+            ans++;
+
+            if (i < n - 1 && s[i] === s[i + 1]) {
+                dp[i][i + 1] = true;
+                ans++;
+            }
+        }
+
+        for (let len = 3; len <= n; len++){
+            for (let i=0; i <= n-len; i++){
+                let j = i + len-1;
+
+                if((s[i] === s[j]) && (dp[i+1][j-1] === true)){
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
